a5aa06f491b5089eba67fc238ca71133eff1ae3d
aidanspalding1@gmail.com <aidanspalding1@gmail.com>
2021-04-22 14:53:38 +0100

    Fixed popup dialog for contact us

--- javascript/contactus.js
+++ javascript/contactus.js
@@ -16,7 +16,10 @@ function getUserMessage() {
 
 //Line break solution from https://stackoverflow.com/questions/9739354/javascript-email-linebreaks
 function prepareEmail() {
+    window.alert("Thank you for your message. Your default email client will now open.")
     window.open("mailto:40434053@live.napier.ac.uk?subject=Suggestions/Issues&body=User Email: " + getUserEmail() + "%0D%0A"
     + "User's Name: " + getUserName() + "%0D%0A"
     + "User's message: %0D%0A" + getUserMessage());
+
+    window.alert("Thank ")
 }
